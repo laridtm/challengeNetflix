121dca4abdb2b26928ffad675ec7b206178963b5
Larissa <>
2020-03-02 17:27:29 -0300

    class Movie like protocol Encodable

--- challangeNetflix/Movie.swift
+++ challangeNetflix/Movie.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-class Movie: Codable {
+struct Movie: Codable {
 
     let title: String
     let year: String
@@ -29,35 +29,55 @@ class Movie: Codable {
     let hdr: Bool
     let images: [String]
     
-    init(title: String, year: String, rated: String, released: String, runtime: String, genre: String, director: String, writer: String, actors: String, plot: String, language:String, country: String, awards: String, poster: String, metascore: String, resolution: Bool, hdr: Bool, images: [String]) {
-        self.title = title
-        self.year = year
-        self.rated = rated
-        self.released = released
-        self.runtime = runtime
-        self.genre = genre
-        self.director = director
-        self.writer = writer
-        self.actors = actors
-        self.plot = plot
-        self.language = language
-        self.country = country
-        self.awards = awards
-        self.poster = poster
-        self.metascore = metascore
-        self.resolution = resolution
-        self.hdr = hdr
-        self.images = images
-    }
+//    init(title: String, year: String, rated: String, released: String, runtime: String, genre: String, director: String, writer: String, actors: String, plot: String, language:String, country: String, awards: String, poster: String, metascore: String, resolution: Bool, hdr: Bool, images: [String]) {
+//        self.title = title
+//        self.year = year
+//        self.rated = rated
+//        self.released = released
+//        self.runtime = runtime
+//        self.genre = genre
+//        self.director = director
+//        self.writer = writer
+//        self.actors = actors
+//        self.plot = plot
+//        self.language = language
+//        self.country = country
+//        self.awards = awards
+//        self.poster = poster
+//        self.metascore = metascore
+//        self.resolution = resolution
+//        self.hdr = hdr
+//        self.images = images
+//    }
     
     enum CodingKeys: String, CodingKey {
-        case Resolution = "4k"
+        case title, year, rated, released, runtime, genre, director, writer,
+        actors, plot, language, country, awards, poster, metascore
+        case resolution = "4k"
+        case hdr, images
+        
     }
     
-    required init(from Decoder: Decoder) throws {
-//        let values = try decoder.container(keyedBy: CodingKeys.self)
-      
-        
+    init(from Decoder: Decoder) throws {
+        let values = try Decoder.container(keyedBy: CodingKeys.self)
+        title = try values.decode(String.self, forKey: .title)
+        year = try values.decode(String.self, forKey: .year)
+        rated = try values.decode(String.self, forKey: .rated)
+        released = try values.decode(String.self, forKey: .released)
+        runtime = try values.decode(String.self, forKey: .runtime)
+        genre = try values.decode(String.self, forKey: .genre)
+        director = try values.decode(String.self, forKey: .director)
+        writer = try values.decode(String.self, forKey: .writer)
+        actors = try values.decode(String.self, forKey: .actors)
+        plot = try values.decode(String.self, forKey: .plot)
+        language = try values.decode(String.self, forKey: .language)
+        country = try values.decode(String.self, forKey: .country)
+        awards = try values.decode(String.self, forKey: .awards)
+        poster = try values.decode(String.self, forKey: .poster)
+        metascore = try values.decode(String.self, forKey: .metascore)
+        resolution = try values.decode(Bool.self, forKey: .resolution)
+        hdr = try values.decode(Bool.self, forKey: .hdr)
+        images = try values.decode([String].self, forKey: .images)
     }
 
 }
