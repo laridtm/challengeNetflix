386d6d0f3b827e6978783d2fdfc8e66bb256eea8
Larissa <>
2020-03-19 14:14:50 -0300

    removed Pods

--- Pods/Target Support Files/Pods-challangeNetflix-challangeNetflixUITests/Pods-challangeNetflix-challangeNetflixUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_challangeNetflix_challangeNetflixUITests : NSObject
-@end
-@implementation PodsDummy_Pods_challangeNetflix_challangeNetflixUITests
-@end
--- Pods/Target Support Files/Pods-challangeNetflix/Pods-challangeNetflix-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_challangeNetflix : NSObject
-@end
-@implementation PodsDummy_Pods_challangeNetflix
-@end
